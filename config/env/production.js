5415b763d68ff6a89cf0c6604a68a3f77a946e4b
Ali Karami <Ali_karami_m@yahoo.com>
2016-11-18 15:03:58 +0330

    set production port to 80

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
@@ -35,5 +35,5 @@ module.exports = {
   //   level: "silent"
   // }
 
-  appUrl: "192.168.1.104:1337"
+  appUrl: "192.168.1.104"
 };
