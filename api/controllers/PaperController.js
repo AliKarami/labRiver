21f4738c3d093b1dcad06998924918f955a334d0
Ali Karami <Ali_karami_m@yahoo.com>
2016-11-20 18:55:33 +0330

    FileService.uploadFile added, FileController Deleted

--- api/controllers/PaperController.js
+++ api/controllers/PaperController.js
@@ -18,41 +18,10 @@ module.exports = {
     return res.view("Resources/Paper",ret)
   },
   createNew : function (req, res) {
-      var documentId = new Promise(function (resolve, reject) {
-        req.file('document').upload({
-          // don't allow the total upload size to exceed ~25MB
-          maxBytes: 25000000,
-          dirname: require('path').resolve(sails.config.appPath, 'assets/files/document'),
-          saveAs: function (__newFileStream, cb) {
-            cb(null, 'paper-document-' + new Date().getTime() + require('path').extname(__newFileStream.filename));
-          }
-        },function whenDone(err, uploadedFiles) {
-          if (err) {
-            reject(err);
-            return res.negotiate(err);
-          }
-          if (uploadedFiles.length > 0) {
-            var _fd = uploadedFiles[0].fd.split('/');
-            var filename = _fd[_fd.length-1];
-            File.create({
-              type: 'document',
-              fileFd: uploadedFiles[0].fd,
-              fileUrl: require('util').format('%s/files/document/%s', sails.config.appUrl, filename),
-              uploader: req.user.id,
-            }).exec(function (err,file) {
-              if (err) {
-                reject(err);
-                return res.negotiate(err);
-              }
-              else resolve(file.id);
-            });
-          } else {
-            //no file upload
-            resolve(undefined);
-          }
-        });
-      })
-      Promise.all([documentId]).then(function (fileIds) {
+    var documentId = FileService.uploadFile(req, 'document');
+    var datasetId = FileService.uploadFile(req, 'dataset');
+    var sourceCodeId = FileService.uploadFile(req, 'source')
+      Promise.all([documentId,datasetId,sourceCodeId]).then(function (fileIds) {
         Paper.create({
           type: req.param('type'),
           title: req.param('title'),
@@ -60,6 +29,8 @@ module.exports = {
           year: req.param("year"),
           abstract: req.param("abstract"),
           document: fileIds[0],
+          dataset: fileIds[1],
+          sourceCode: fileIds[2],
           tags: req.param("tags").split(',')
         }).exec(function (err, paper) {
           if (err) return res.negotiate(err);
--- api/services/FileService.js
+++ api/services/FileService.js
@@ -8,5 +8,39 @@ module.exports = {
         }
       })
     });
-  }
+  },
+  uploadFile: function (req, type) {
+  return new Promise(function (resolve, reject) {
+    req.file(type).upload({
+      maxBytes: 50000000,
+      dirname: require('path').resolve(sails.config.appPath, 'assets/files/' + type),
+      saveAs: function (__newFileStream, cb) {
+        cb(null, 'paper-' + new Date().getTime() + require('path').extname(__newFileStream.filename));
+      }
+    },function whenDone(err, uploadedFiles) {
+      if (err) {
+        reject(err);
+        return res.negotiate(err);
+      }
+      if (uploadedFiles.length > 0) {
+        var _fd = uploadedFiles[0].fd.split('/');
+        var filename = _fd[_fd.length-1];
+        File.create({
+          type: type,
+          fileFd: uploadedFiles[0].fd,
+          fileUrl: require('util').format('%s/files/%s/%s', sails.config.appUrl, type, filename),
+          uploader: req.user.id,
+        }).exec(function (err,file) {
+          if (err) {
+            reject(err);
+            return res.negotiate(err);
+          }
+          else resolve(file.id);
+        });
+      } else {
+        resolve(undefined);
+      }
+    });
+  });
+}
 };
