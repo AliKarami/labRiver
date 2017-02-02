3838df986195ebcdb1c0c3eeb8546169bb4edf81
Ali Karami <Ali_karami_m@yahoo.com>
2017-02-02 22:18:42 +0330

    migrate:safe database

--- config/connections.js
+++ config/connections.js
@@ -62,7 +62,8 @@ module.exports.connections = {
     port: 27017,
     // user: 'username', //optional
     // password: 'password', //optional
-    database: 'labRiver' //optional
+    database: 'labRiver', //optional
+    migrate: 'safe'
   }
   // ,
 
